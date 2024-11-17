3c385a35541ab1677c9477260649de9847c58e88
tonnatsementu66 <tonnatsementu@gmail.com>
2024-11-17 12:55:43 -0500

    added planet 2

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -12,9 +12,9 @@ public class SolarSystem {
         Archilies.size="50meters";
         Archilies.temp="90000000";
 
-        planet2 nobel =new planet2();
-        nobel.temp="10000";
-        nobel.color="black";
+        planet2 nobel2 =new planet2();
+        nobel2.temp="10000";
+        nobel2.color="black";
 
         
     }
